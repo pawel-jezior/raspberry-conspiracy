e94cacee878860cca391e3fa20653d62549f267c
Pawel Jezior <paweljfrontend@gmail.com>
2024-06-26 19:09:45 +0200

    version 1.2 add image to in_progress state

--- src/components/Medal/Medal.js
+++ src/components/Medal/Medal.js
@@ -1,5 +1,6 @@
 import "./Medal.css";
 import win from "../../media/images/win.png";
+import progress from "../../media/images/progress.png";
 import { useEffect } from "react";
 
 export const Medal = ({ pointsP, pointsB }) => {
@@ -8,6 +9,7 @@ export const Medal = ({ pointsP, pointsB }) => {
     const medal = document.getElementById("medal");
     const bottomText = document.getElementById("nextWeekText");
     const image = document.getElementById("image");
+    const imageProgress =  document.getElementById("imageProgress");
     const line = document.getElementById("line");
     if (
       upperText &&
@@ -21,6 +23,7 @@ export const Medal = ({ pointsP, pointsB }) => {
       upperText.style.marginTop = "0";
       bottomText.style.display = "block";
       image.style.display = "block";
+      imageProgress.style.display = "none";
       line.style.display = "block";
       medal.style.border = "4px solid #07cc00";
     } else if (
@@ -36,6 +39,7 @@ export const Medal = ({ pointsP, pointsB }) => {
       upperText.style.marginTop = "-6px";
       bottomText.style.display = "none";
       image.style.display = "none";
+      imageProgress.style.display = "block";
       line.style.display = "none";
       medal.style.border = "4px solid #ffd900";
     } else if (
@@ -50,6 +54,7 @@ export const Medal = ({ pointsP, pointsB }) => {
       upperText.style.marginTop = "0";
       bottomText.style.display = "none";
       image.style.display = "none";
+      imageProgress.style.display = "none";
       line.style.display = "none";
       medal.style.border = "4px solid #ff007f";
     }
@@ -61,6 +66,7 @@ export const Medal = ({ pointsP, pointsB }) => {
         WE DID IT!
       </div>
       <img id="image" className="image" src={win} alt="win" />
+      <img id="imageProgress" className="imageProgress" src={progress} alt="win" />
       <span id="line" className="line" />
       <div id="nextWeekText" className="nextWeekText">
         See you next week!
